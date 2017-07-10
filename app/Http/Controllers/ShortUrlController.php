c5d76b8bad64080cc971fd4a56f5d410302ce953
smilexxxx <smile.nikitoz@gmail.com>
2017-07-11 00:19:52 +0300

    Исправил редирект после авторизации

--- app/Http/Controllers/ShortUrlController.php
+++ app/Http/Controllers/ShortUrlController.php
@@ -10,7 +10,7 @@ class ShortUrlController extends Controller
     public function index($short_url){
         $shortUrl = ShortUrl::where('short_url', $short_url)->first();
         if (is_null($shortUrl)){
-            return redirect('/not-found');
+            return redirect(route('create_short_url'));
         }else{
             $shortUrl->views += 1;
             $shortUrl->save();
